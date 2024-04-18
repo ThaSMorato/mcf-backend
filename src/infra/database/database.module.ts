9ef68e64c076f743d92d22f99a38454745b61e4a
ThaSMorato <tha.s.morato@gmail.com>
2024-04-18 18:19:32 -0300

    Chore: add prisma shifts repo in database module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
 
+import { ShiftsRepository } from '@/mfc/application/repositories/shifts-repository'
+
 import { PrismaService } from './prisma/prisma.service'
+import { PrismaShiftsRepository } from './prisma/repositories/prisma-shifts-repository'
 
 @Module({
-  providers: [PrismaService],
-  exports: [],
+  providers: [
+    PrismaService,
+    {
+      provide: ShiftsRepository,
+      useClass: PrismaShiftsRepository,
+    },
+  ],
+  exports: [PrismaShiftsRepository],
 })
 export class DatabaseModule {}
