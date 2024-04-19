db59c4023793dea6bb8f13e387dbceb8338f9084
ThaSMorato <tha.s.morato@gmail.com>
2024-04-18 22:57:30 -0300

    Chore: seeder, dotenv, setup e2e and docker image fix

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
+import { SeederService } from './database/prisma/seed/seeder.service'
 import { EnvService } from './env/env.service'
 
 async function bootstrap() {
@@ -8,6 +9,17 @@ async function bootstrap() {
 
   const envService = app.get(EnvService)
   const port = envService.get('PORT')
+  const nodeEnv = envService.get('NODE_ENV')
+
+  if (nodeEnv === 'DEV') {
+    const seederService = app.get(SeederService)
+    await seederService.checkAndSeed()
+  }
+
+  app.enableCors({
+    origin: true, // This can be a specific URL or true to allow all origins
+    credentials: true, // This allows cookies to be sent
+  })
 
   await app.listen(port)
 }
